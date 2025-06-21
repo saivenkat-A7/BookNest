a17d253c7726afa332a2a4fb2ff422f1e1ab962a
Sai Venkat <saivenkatkankipati456@gmail.com>
2025-06-21 07:16:11 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://book-nest-xwrc.vercel.app'],
+    origin: ['http://localhost:5173', 'https://book-nest-xwrc.vercel.app/'],
     credentials: true
 }))
 
