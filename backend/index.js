9d51ce65576a2a3d541ff82a3291b0c301b8b953
Sai Venkat <saivenkatkankipati456@gmail.com>
2025-06-22 17:17:27 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require('dotenv').config()
 // middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['http://localhost:5173'],
+    origin: ['http://localhost:5173','https://booknest-8-60yz.onrender.com'],
     credentials: true
 }))
 
